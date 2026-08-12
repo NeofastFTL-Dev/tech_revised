09e2f91f19a1b6d73b036603e2aab2a49d5c5cff
NeofastFTL <stefanovandermeer.bussiness@outlook.com>
2026-08-12 20:50:49 +0200

    Orient EAF input bus to face the player

--- src/main/java/com/neofast/tech_revised/block/custom/ElectricArcFurnaceInputBusBlock.java
+++ src/main/java/com/neofast/tech_revised/block/custom/ElectricArcFurnaceInputBusBlock.java
@@ -42,7 +42,7 @@ public class ElectricArcFurnaceInputBusBlock extends BaseEntityBlock {
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection());
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
     @Override
