9319b2cf891437d6c5684ed22a0108edd31dab65
NeofastFTL <stefanovandermeer.bussiness@outlook.com>
2026-08-13 08:35:30 +0200

    Creative tab and lang entries for RAM stick chain

--- src/main/java/com/neofast/tech_revised/item/ModCreativeModeTabs.java
+++ src/main/java/com/neofast/tech_revised/item/ModCreativeModeTabs.java
@@ -15,18 +15,9 @@ public class ModCreativeModeTabs {
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TechRevised.MOD_ID);
 
     public static final RegistryObject<CreativeModeTab> TECH_REVISED_TAB = CREATIVE_MODE_TABS.register("tech_revised_tab",
-            () -> CreativeModeTab.builder()
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.STEEL_INGOT.get()))
                     .title(Component.translatable("creativetab.tech_revised_tab"))
-                    .icon(() -> new ItemStack(ModBlocks.ELECTRIC_ARC_FURNACE_CONTROLLER.get()))
                     .displayItems((parameters, output) -> {
-                        output.accept(ModBlocks.GRAPHITE_ORE.get());
-                        output.accept(ModItems.GRAPHITE_DUST.get());
-                        output.accept(ModItems.ELECTRODE.get());
-                        output.accept(ModBlocks.CHROMITE_ORE.get());
-                        output.accept(ModItems.CHROMITE_DUST.get());
-                        output.accept(ModBlocks.CRUSHER.get());
-                        output.accept(ModBlocks.WINDOWS_7_VM_BLOCK.get());
-                        output.accept(ModBlocks.STEEL_BLOCK.get());
                         output.accept(ModItems.STEEL_INGOT.get());
                         output.accept(ModItems.STEEL_NUGGET.get());
                         output.accept(ModBlocks.STAINLESS_STEEL_BLOCK.get());
@@ -58,31 +49,26 @@ public class ModCreativeModeTabs {
                         output.accept(ModBlocks.OXYGEN_CONVERTER_CONTROLLER.get());
                         output.accept(ModBlocks.OXYGEN_CONVERTER_FRAME.get());
                         output.accept(ModItems.COAL_COKE.get());
-                        output.accept(ModItems.HEAVY_CRUDE_OIL_BUCKET.get());
-                        output.accept(ModItems.CREOSOTE_BUCKET.get());
-                        output.accept(ModItems.OXYGEN_BUCKET.get());
-                        output.accept(ModItems.HYDROGEN_BUCKET.get());
-                        output.accept(ModItems.LIQUID_GLASS_BUCKET.get());
-                        output.accept(ModItems.SODIUM_CARBONATE_SOLUTION_BUCKET.get());
-                        output.accept(ModItems.CUPRIC_CHLORIDE_SOLUTION_BUCKET.get());
-                        output.accept(ModItems.SODIUM_HYDROXIDE_SOLUTION_BUCKET.get());
-
-                        // Stainless Steel Chain
-                        output.accept(ModBlocks.BLAST_FURNACE_CORE.get());
+                        output.accept(ModItems.ELECTRODE.get());
+                        output.accept(ModItems.GRAPHITE_DUST.get());
                         output.accept(ModItems.CHROMIUM_DUST.get());
                         output.accept(ModItems.NICKEL_DUST.get());
                         output.accept(ModItems.CARBON_CHUNK.get());
                         output.accept(ModItems.STAINLESS_STEEL_BLEND.get());
-
-                        // Fiberglass Chain
-                        output.accept(ModBlocks.INDUSTRIAL_BATCHING_MIXER.get());
-                        output.accept(ModBlocks.REFRACTORY_MELTING_FURNACE.get());
-                        output.accept(ModBlocks.CHEMICAL_SIZING_APPLICATOR.get());
-                        output.accept(ModBlocks.HIGH_SPEED_GATHERING_WINDER.get());
-                        output.accept(ModBlocks.INDUSTRIAL_DRYING_OVEN.get());
-                        output.accept(ModBlocks.ROVING_CREEL_CONVERTER.get());
-                        output.accept(ModBlocks.INDUSTRIAL_TEXTILE_WEAVING_LOOM.get());
-                        output.accept(ModBlocks.STRAND_CHOPPING_MACHINERY.get());
+                        output.accept(ModItems.CHROMITE_DUST.get());
+                        output.accept(ModBlocks.CRUSHER.get());
+                        output.accept(ModBlocks.BATCHING_MIXER.get());
+                        output.accept(ModBlocks.MELTING_FURNACE.get());
+                        output.accept(ModBlocks.BUSHING_TIP_ASSEMBLY.get());
+                        output.accept(ModBlocks.FIBER_ATTENUATION_UNIT.get());
+                        output.accept(ModBlocks.SIZE_APPLICATOR.get());
+                        output.accept(ModBlocks.COLLET_WINDER.get());
+                        output.accept(ModBlocks.DRYING_OVEN.get());
+                        output.accept(ModBlocks.ROVING_CREEL.get());
+                        output.accept(ModBlocks.AIR_JET_TEXTURIZER.get());
+                        output.accept(ModBlocks.WEAVING_LOOM.get());
+                        output.accept(ModBlocks.CHOPPER.get());
+                        output.accept(ModBlocks.MAT_FORMER.get());
                         output.accept(ModItems.SILICA_SAND.get());
                         output.accept(ModItems.LIMESTONE_DUST.get());
                         output.accept(ModItems.DOLOMITE_DUST.get());
@@ -99,24 +85,23 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.FIBERGLASS_FABRIC.get());
                         output.accept(ModItems.CHOPPED_GLASS_STRAND.get());
                         output.accept(ModItems.CHOPPED_STRAND_MAT.get());
-
-                        // PCB Chain
+                        output.accept(ModItems.LIQUID_GLASS_BUCKET.get());
                         output.accept(ModBlocks.DESIGN_ENGINEERING_STATION.get());
-                        output.accept(ModBlocks.INDUSTRIAL_SHEAR.get());
-                        output.accept(ModBlocks.DECONTAMINATION_OVEN.get());
-                        output.accept(ModBlocks.HYDRAULIC_CLADDING_PRESS.get());
+                        output.accept(ModBlocks.SUBSTRATE_PREPARATION_UNIT.get());
+                        output.accept(ModBlocks.COPPER_CLADDING_PRESS.get());
                         output.accept(ModBlocks.PHOTO_RESIST_APPLICATOR.get());
                         output.accept(ModBlocks.UV_LDI_IMAGER.get());
                         output.accept(ModBlocks.CHEMICAL_DEVELOPING_WASH.get());
                         output.accept(ModBlocks.ACIDIC_ETCHING_SPRAYER.get());
                         output.accept(ModBlocks.ALKALINE_STRIPPING_STATION.get());
+                        output.accept(ModBlocks.AOI_VERIFICATION_STATION.get());
                         output.accept(ModBlocks.VACUUM_LAMINATION_PRESS.get());
                         output.accept(ModBlocks.XRAY_ALIGNMENT_DRILL.get());
-                        output.accept(ModBlocks.ELECTROLESS_PLATING_BATH.get());
-                        output.accept(ModBlocks.COPPER_ELECTROPLATING_TANK.get());
-                        output.accept(ModBlocks.TIN_PLATING_TANK.get());
+                        output.accept(ModBlocks.ELECTROLESS_COPPER_BATH.get());
+                        output.accept(ModBlocks.ELECTROPLATING_CELL.get());
+                        output.accept(ModBlocks.TIN_PLATING_LINE.get());
                         output.accept(ModBlocks.SOLDERMASK_FLOODER.get());
-                        output.accept(ModBlocks.UV_PAD_EXPOSURE_STATION.get());
+                        output.accept(ModBlocks.UV_PAD_EXPOSURE_UNIT.get());
                         output.accept(ModBlocks.INKJET_SILKSCREEN_PRINTER.get());
                         output.accept(ModBlocks.SURFACE_FINISH_STATION.get());
                         output.accept(ModBlocks.FLYING_PROBE_TESTER.get());
@@ -124,8 +109,6 @@ public class ModCreativeModeTabs {
                         output.accept(ModBlocks.SOLDER_PASTE_PRINTER.get());
                         output.accept(ModBlocks.PICK_AND_PLACE_ROBOT.get());
                         output.accept(ModBlocks.MULTI_ZONE_REFLOW_OVEN.get());
-                        output.accept(ModBlocks.AOI_VERIFICATION_STATION.get());
-
                         output.accept(ModItems.CAD_LAYOUT.get());
                         output.accept(ModItems.VERIFIED_GERBER_FILE.get());
                         output.accept(ModItems.RAW_FR4_LAMINATE.get());
@@ -137,8 +120,11 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.INNER_LAYER_PATTERNED_BOARD.get());
                         output.accept(ModItems.DEVELOPED_INNER_LAYER_BOARD.get());
                         output.accept(ModItems.SODIUM_CARBONATE_DUST.get());
+                        output.accept(ModItems.SODIUM_CARBONATE_SOLUTION_BUCKET.get());
                         output.accept(ModItems.CUPRIC_CHLORIDE_DUST.get());
+                        output.accept(ModItems.CUPRIC_CHLORIDE_SOLUTION_BUCKET.get());
                         output.accept(ModItems.SODIUM_HYDROXIDE_DUST.get());
+                        output.accept(ModItems.SODIUM_HYDROXIDE_SOLUTION_BUCKET.get());
                         output.accept(ModItems.ETCHED_INNER_LAYER_BOARD.get());
                         output.accept(ModItems.STRIPPED_INNER_LAYER_BOARD.get());
                         output.accept(ModItems.PREPREG_FABRIC.get());
@@ -163,6 +149,10 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.PLACED_PCBA.get());
                         output.accept(ModItems.REFLOWED_PCBA.get());
                         output.accept(ModItems.FINAL_PCBA.get());
+                        output.accept(ModItems.DRAM_PACKAGE.get());
+                        output.accept(ModItems.SPD_EEPROM.get());
+                        output.accept(ModItems.UNPROGRAMMED_RAM_MODULE.get());
+                        output.accept(ModItems.RAM_STICK.get());
                     })
                     .build());
 
